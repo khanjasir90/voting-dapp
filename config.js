80047122f51ba595c08e14f2ca655ca1921ad8a0
khanjasir90 <khanmohdjasir@gmail.com>
2022-03-14 13:01:47 +0530

    Integrated Smart Contract with Web3

---
+++ config.js
@@ -0,0 +1,110 @@
+
+const CONTACT_ADDRESS = '0xe13B067cC316a2ab3dB64B3E4817D88c176f1f7F';
+
+const CONTACT_ABI = [
+    {
+        "inputs": [
+          {
+            "internalType": "uint256",
+            "name": "",
+            "type": "uint256"
+          }
+        ],
+        "name": "candidates",
+        "outputs": [
+          {
+            "internalType": "uint256",
+            "name": "id",
+            "type": "uint256"
+          },
+          {
+            "internalType": "string",
+            "name": "name",
+            "type": "string"
+          },
+          {
+            "internalType": "uint256",
+            "name": "age",
+            "type": "uint256"
+          },
+          {
+            "internalType": "uint256",
+            "name": "voteCount",
+            "type": "uint256"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function",
+        "constant": true
+      },
+      {
+        "inputs": [],
+        "name": "candidatesCount",
+        "outputs": [
+          {
+            "internalType": "uint256",
+            "name": "",
+            "type": "uint256"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function",
+        "constant": true
+      },
+      {
+        "inputs": [
+          {
+            "internalType": "address",
+            "name": "",
+            "type": "address"
+          }
+        ],
+        "name": "voters",
+        "outputs": [
+          {
+            "internalType": "bool",
+            "name": "",
+            "type": "bool"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function",
+        "constant": true
+      },
+      {
+        "inputs": [
+          {
+            "internalType": "string",
+            "name": "_name",
+            "type": "string"
+          },
+          {
+            "internalType": "uint256",
+            "name": "_age",
+            "type": "uint256"
+          }
+        ],
+        "name": "addCandidate",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+      {
+        "inputs": [
+          {
+            "internalType": "uint256",
+            "name": "_id",
+            "type": "uint256"
+          }
+        ],
+        "name": "vote",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }
+];
+
+module.exports = {
+    CONTACT_ABI,
+    CONTACT_ADDRESS
+};
--- server.js
+++ server.js
@@ -1,10 +1,38 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const Web3 = require('web3');
+const contract = require('@truffle/contract');
+const artifacts = require('./build/contracts/Voting.json');
+const CONTACT_ABI = require('./config');
+const CONTACT_ADDRESS = require('./config');
+
+app.use(cors());
+app.use(express.json());
+
+if (typeof web3 !== 'undefined') {
+    var web3 = new Web3(web3.currentProvider); 
+} else {
+    var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:7545'));
+}
 
 
-app.post('/vote/:id',(req,res) => {
-    const candidateId = req.params.id;
-    console.log(candidateId);
+app.get('/',async (req,res) => {   
+    const votingContractObj = new web3.eth.Contract(CONTACT_ABI.CONTACT_ABI,CONTACT_ADDRESS.CONTACT_ADDRESS);
+    const candidatesData = await votingContractObj.methods.candidates(1).call({from: '0x08b27fC6122d79FE1833ceC7Aa90e517d200CEd5'});
+    console.log(candidatesData);
+})
+
+app.post('/addCandidate', async(req,res) => {
+    const votingContractObj = new web3.eth.Contract(CONTACT_ABI.CONTACT_ABI,CONTACT_ADDRESS.CONTACT_ADDRESS);
+    const ifAdded = await votingContractObj.methods.addCandidate('Jasir',20).send({from: '0x08b27fC6122d79FE1833ceC7Aa90e517d200CEd5', gas:3000000});
+    console.log(ifAdded);
+})
+
+app.post('/vote',async(req,res) => {
+    const votingContractObj = new web3.eth.Contract(CONTACT_ABI.CONTACT_ABI,CONTACT_ADDRESS.CONTACT_ADDRESS);
+    const ifAdded = await votingContractObj.methods.vote(1).send({from: '0x0408f48B82f71d2CCE0765159e97DFa088e579C8',gas: 300000})
+    console.log(ifAdded);
 })
 
 
