ff28fb7b953ee4127fcd75904c00f866d59c7be7
khanjasir90 <khanmohdjasir@gmail.com>
2022-03-19 14:21:16 +0530

    added apis for vote add candidate and getall candidate

--- server.js
+++ server.js
@@ -4,36 +4,60 @@ const cors = require('cors');
 const Web3 = require('web3');
 const contract = require('@truffle/contract');
 const artifacts = require('./build/contracts/Voting.json');
-const CONTACT_ABI = require('./config');
-const CONTACT_ADDRESS = require('./config');
+const Contract = new require('./Contract');
+const Provider = new require('./Provider');
+
+const contractt = new Contract();
+const provider = new Provider();
+const web3 = provider.web3;
+const instance = contractt.initContract();
+
+
+app.set('views','./views')
+app.set('view engine','ejs')
 
 app.use(cors());
 app.use(express.json());
 
-if (typeof web3 !== 'undefined') {
-    var web3 = new Web3(web3.currentProvider); 
-} else {
-    var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:7545'));
-}
 
-
-app.get('/',async (req,res) => {   
-    const votingContractObj = new web3.eth.Contract(CONTACT_ABI.CONTACT_ABI,CONTACT_ADDRESS.CONTACT_ADDRESS);
-    const candidatesData = await votingContractObj.methods.candidates(1).call({from: '0x08b27fC6122d79FE1833ceC7Aa90e517d200CEd5'});
-    console.log(candidatesData);
+app.get('/',async (req,res) => {
+    const accounts = await web3.eth.getAccounts();
+    const candidatesCount = await instance.methods.candidatesCount().call();
+    const alreadyVoted = await instance.methods.voters(accounts[0]).call();
+    console.log(alreadyVoted);
+    var candidateObject = [];
+    for(var i=1;i<=candidatesCount;i++) {
+        const temp = await instance.methods.candidates(i).call();
+        candidateObject.push({...temp});
+    }
+    console.log(candidateObject);
+    res.render('index', {
+        data:candidateObject,
+        address:accounts[0],
+        alreadyVoted: alreadyVoted
+    })
 })
 
-app.post('/addCandidate', async(req,res) => {
-    const votingContractObj = new web3.eth.Contract(CONTACT_ABI.CONTACT_ABI,CONTACT_ADDRESS.CONTACT_ADDRESS);
-    const ifAdded = await votingContractObj.methods.addCandidate('Jasir',20).send({from: '0x08b27fC6122d79FE1833ceC7Aa90e517d200CEd5', gas:3000000});
-    console.log(ifAdded);
+app.get('/vote/:id', async(req,res) => {
+    const id = req.params.id;
+    try {
+        const accounts = await web3.eth.getAccounts();
+        await instance.methods.vote(id).send({from: accounts[0], gas:300000})
+        res.redirect('/');
+    } catch(err) {
+        console.log(err);
+    }
+
 })
 
-app.post('/vote',async(req,res) => {
-    const votingContractObj = new web3.eth.Contract(CONTACT_ABI.CONTACT_ABI,CONTACT_ADDRESS.CONTACT_ADDRESS);
-    const ifAdded = await votingContractObj.methods.vote(1).send({from: '0x0408f48B82f71d2CCE0765159e97DFa088e579C8',gas: 300000})
-    console.log(ifAdded);
+app.post('/addCandidate',async(req,res) => {
+    try {
+        const accounts = await web3.eth.getAccounts();
+        await instance.methods.addCandidate('Abdullah',21).send({from: accounts[0],gas: 300000});
+        res.redirect('/')
+    } catch (err) {
+        console.log(err);
+    }
 })
-
 
 app.listen(3000,() => console.log('Server Running on port 3000'));
